8f34599f8e6f9f336e564089455466b00a92a9c2
krishnapriya <krishnapriyakamesh@gmail.com>
2017-01-10 14:25:31 -0600

    Done till getting data from api

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
 console.log("Start");
+var movieName ="";
+var movieList = "";
+
+$("#firstLook").show();
+$(".movieList").hide();
+
+$(".card__link").click(function(e){
+var nextCard = $(this).attr("next");
+
+
+switch (nextCard) {
+  case "movieList":
+   $("#fixedNav").show()
+    $("."+nextCard).show()
+    $("#search").click( function () {
+     movieName = $("input").val();
+     console.log(movieName)
+     movieFactory()
+    })
+       break;
+
+    }
+})
+///ajax call for API
+   function movieFactory () {
+     return new Promise (function (resolve,reject){
+      $.ajax({
+        url: "http://www.omdbapi.com/?s=" + movieName
+      })
+
+      .then (function(data){
+        resolve(data)
+        console.log(data)
+        for (var i=0 ; i < data.Search.length; i++){
+          movieList += `<div><h3>Title: ${data.Search[i].Title}</h3>
+                             <p>Year: ${data.Search[i].Year}</p>
+                            </div>`
+
+          $("#movieList").html (movieList);
+        }
+      })
+    })
+}
