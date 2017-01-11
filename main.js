b846f30a24548eb32d69a5d2d753c26e96fd1da5
John McCutchan <john.mccutchan.wd@gmail.com>
2017-01-10 21:34:27 -0600

    reworked factory and added a call to the card function

--- main.js
+++ main.js
@@ -1,44 +1,32 @@
+/*
+Team Excercise Cohort 17
+John McCutchan
+Krishnapriya Sivasubramanian
+James Wier
+*/
 console.log("Start");
+//
+/* Globals ============================================= */
 var movieName ="";
-var movieList = "";
-
-$("#firstLook").show();
-$(".movieList").hide();
-
-$(".card__link").click(function(e){
-var nextCard = $(this).attr("next");
-
-
-switch (nextCard) {
-  case "movieList":
-   $("#fixedNav").show()
-    $("."+nextCard).show()
-    $("#search").click( function () {
-     movieName = $("input").val();
-     console.log(movieName)
-     movieFactory()
-    })
-       break;
-
-    }
+//
+/* Search listener ===================================== */
+$("#search").click( function () {
+  movieName = $("input").val();
+  console.log(movieName);
+  movieFactory();
 })
-///ajax call for API
-   function movieFactory () {
-     return new Promise (function (resolve,reject){
-      $.ajax({
-        url: "http://www.omdbapi.com/?s=" + movieName
-      })
-
-      .then (function(data){
-        resolve(data)
-        console.log(data)
-        for (var i=0 ; i < data.Search.length; i++){
-          movieList += `<div><h3>Title: ${data.Search[i].Title}</h3>
-                             <p>Year: ${data.Search[i].Year}</p>
-                            </div>`
-
-          $("#movieList").html (movieList);
-        }
-      })
+//
+/* Ajax call for API =================================== */
+function movieFactory () {
+  return new Promise (function (resolve,reject){
+    $.ajax({
+      url: "http://www.omdbapi.com/?s=" + movieName
     })
+    .then (function(data){
+      resolve(data)
+      console.log(data)
+      card(data);
+      $("#movieList").html (movieList);
+    })
+  })
 }
