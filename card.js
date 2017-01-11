f139a1feb9cb8d19726502f367f28881b30f58f4
krishnapriya <krishnapriyakamesh@gmail.com>
2017-01-11 15:03:11 -0600

    added database in the firebase for the watched

--- card.js
+++ card.js
@@ -16,10 +16,12 @@ function card(data) {
   for (var i=0 ; i < data.Search.length; i++){
 
     movieList += `
-                  <div class="col-md-4 movieCard">
+                  <div class="col-md-4 movieCard" id="movieCard${i}">
                     <h3>${data.Search[i].Title}</h3>
                     <p>Year: ${data.Search[i].Year}</p>
                     <img class="img-responsive center-block" src= ${data.Search[i].Poster} alt="Poster not available " />
+                    <p><a href="movieCard${i}" rel="modal:open"></a></p>
+
                   </div>`
   }
   return movieList;
@@ -34,6 +36,7 @@ function modalCardBuilder(data) {
                   <p>Rating: ${data.imdbRating}
                   <p>Cast: ${data.Actors}
                   <img class="img-responsive center-block" src= ${data.Poster} alt="Poster not available " />
-                </div>`
+                </div>
+                <button id="watched">Watched</button> <button id="addToWatchList">Add to WatchList</button>`
   return modalCard;
 }
